3248bf90ef992d7e7a3707b46e2225c69aaa2b22
gaochencheng <1620627099@qq.com>
2023-01-11 16:33:13 +0800

    23.1.11

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # c-_learning
+
+[`ato-x strto-x stoi` 那些事](./string2int)
--- string2int/s2i.cpp
+++ string2int/s2i.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-    const char *str1 = "asq,";
-    int e = strtol(str1, nullptr, 10);
-    cout << e << endl;
-    // int d = stoi(str1); // 报异常
+    //首先atoi和strtol都是c里面的函数，他们都可以将字符串转为int，它们的参数都是const char*，因此在用string时，必须调c_str()方法将其转为char*的字符串。
+    string str1 = "asq,";
+    int e = strtol(str1.c_str(),nullptr,10);
+    int d = stoi(str1); // 报异常
     string str2 = "12312";
     int c = stoi(str2); // ok
     cout << c << endl;
