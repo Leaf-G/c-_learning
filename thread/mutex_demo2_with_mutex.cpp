c0c6203730ed4d72b819dcab617a72c0068fa28b
gaochencheng <1620627099@qq.com>
2023-02-07 13:43:00 +0800

    mutex

---
+++ thread/mutex_demo2_with_mutex.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <chrono>
+#include <stdexcept>
+
+int counter = 0;
+std::mutex mtx; // 保护counter
+
+void increase(int time) {
+    for (int i = 0; i < time; i++) {
+        mtx.lock();
+        // 当前线程休眠1毫秒
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        counter++;
+        mtx.unlock();
+    }
+}
+
+int main(int argc, char** argv) {
+    std::thread t1(increase, 10000);
+    std::thread t2(increase, 10000);
+    t1.join();
+    t2.join();
+    std::cout << "counter:" << counter << std::endl;
+    return 0;
+}
